29dcdc5d59e5d0705d80ae72c1f78f2f6c246189
Michael Markey <michaelamarkey@gmail.com>
2022-12-11 04:12:05 -0500

    fix: refactor to use promise all

--- app/models/matchup.server.ts
+++ app/models/matchup.server.ts
@@ -20,35 +20,47 @@ const fieldsSchema = z.object({
   Items: z.tuple([z.string().optional(), z.string().optional()]),
 });
 
+export async function getBaseMatchups({
+  airtableToken,
+}: {
+  airtableToken: unknown;
+}): Promise<BaseMatchup[]> {
+  const baseMatchupsData = await createRequest(
+    airtableToken,
+    config.airtable.matchupsUrl
+  );
+  const responseSchema = createResponseSchema(fieldsSchema);
+  const parsedBaseMatchups = responseSchema.parse(baseMatchupsData);
+  const baseMatchups = parsedBaseMatchups.records.map((baseMatchup) => {
+    const { fields } = baseMatchup;
+    return {
+      date: fields.Date,
+      id: baseMatchup.id,
+      itemId1: fields.Items[0],
+      itemId2: fields.Items[1],
+    };
+  });
+  return baseMatchups;
+}
+
 export async function getMatchups({
   airtableToken,
 }: {
   airtableToken: unknown;
 }): Promise<Matchup[]> {
-  const matchupsData = await createRequest(
-    airtableToken,
-    config.airtable.matchupsUrl
-  );
-  const responseSchema = createResponseSchema(fieldsSchema);
-  const parsedMatchups = responseSchema.parse(matchupsData);
-  const matchups = parsedMatchups.records.map((matchup) => {
-    const { fields } = matchup;
-    return {
-      date: fields.Date,
-      id: matchup.id,
-      itemId1: fields.Items[0],
-      itemId2: fields.Items[1],
-    };
-  });
+  const [baseMatchups, items] = await Promise.all([
+    getBaseMatchups({ airtableToken }),
+    getItems({ airtableToken }),
+  ]);
 
-  const items = await getItems({ airtableToken });
-  const matchupsWithItems = matchups.map((matchup) => {
-    const { itemId1, itemId2, ...baseMatchup } = matchup;
+  const matchups = baseMatchups.map((baseMatchup) => {
+    const { itemId1, itemId2, ...baseMatchupValues } = baseMatchup;
     return {
-      ...baseMatchup,
+      ...baseMatchupValues,
       item1: items.find((item) => item.id === itemId1),
       item2: items.find((item) => item.id === itemId2),
     };
   });
-  return matchupsWithItems;
+
+  return matchups;
 }
