cd8fd11abc3ee34917a9bff236caf406fb090ea1
Michael Markey <michaelamarkey@gmail.com>
2022-12-19 01:13:31 -0500

    feat: add remaining tabs

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -5,6 +5,7 @@ import { useLoaderData } from '@remix-run/react';
 import { Matchup, matchupStyles } from '~/components';
 import { getMatchups } from '~/models/matchup.server';
 import indexStyles from '~/styles/index.css';
+import brackets from '~/assets/brackets.png';
 
 export const loader = async ({ context }: LoaderArgs) => {
   const cachedMatchups = await context.CWC_KV.get<
@@ -47,53 +48,78 @@ export default function Index() {
     <Tab.Group>
       <Tab.List className="tablist">
         <Tab className="tablistItem">Matchups</Tab>
+        <Tab className="tablistItem">Brackets</Tab>
         <Tab className="tablistItem">Information</Tab>
       </Tab.List>
 
       <Tab.Panels>
-        <Tab.Panel>
-          <div className="index">
-            <div className="indexMatchups">
-              <h2>Ongoing Matches</h2>
-              {ongoingMatches.map((matchup) => (
-                <div key={matchup.id}>
-                  <Matchup {...matchup} />
-                </div>
-              ))}
-            </div>
+        <Tab.Panel className="tabpanel">
+          <div className="indexMatchups">
+            <h2>Ongoing Matches</h2>
+            {ongoingMatches.map((matchup) => (
+              <div key={matchup.id}>
+                <Matchup {...matchup} />
+              </div>
+            ))}
+          </div>
 
-            <div className="indexMatchups">
-              <h2>Recent Matches</h2>
-              {completedMatches.map((matchup) => (
-                <div key={matchup.id}>
-                  <Matchup {...matchup} />
-                </div>
-              ))}
-            </div>
+          <div className="indexMatchups">
+            <h2>Recent Matches</h2>
+            {completedMatches.map((matchup) => (
+              <div key={matchup.id}>
+                <Matchup {...matchup} />
+              </div>
+            ))}
+          </div>
 
-            <div className="indexMatchups">
-              <h2>Future Matches</h2>
-              {futureMatches.map((matchup) => (
-                <div className="indexMatchupsDisabled" key={matchup.id}>
-                  <Matchup {...matchup} />
-                </div>
-              ))}
-            </div>
+          <div className="indexMatchups">
+            <h2>Future Matches</h2>
+            {futureMatches.map((matchup) => (
+              <div className="indexMatchupsDisabled" key={matchup.id}>
+                <Matchup {...matchup} />
+              </div>
+            ))}
           </div>
         </Tab.Panel>
-        <Tab.Panel>
-          <div className="index">
-            <div className="indexLink">
-              <a href="https://twitter.com/conbiniboys/status/1599411940358438913?s=20&t=TrVXXdz6UPGXZUHToYEXYg">
-                Information
-              </a>
-            </div>
 
-            <div className="indexLink">
-              <a href="https://docs.google.com/spreadsheets/u/3/d/1kyKhbq6h-8Qe_RWgavOBb34pHgVkIr2nznjbp8eeszY/copy#gid=0">
-                Download link for Brackets
-              </a>
-            </div>
+        <Tab.Panel className="tabpanel">
+          <h2>Check Out the Brackets</h2>
+          <div className="brackets">
+            <a
+              className="bracketsLink"
+              href="https://docs.google.com/spreadsheets/d/1kyKhbq6h-8Qe_RWgavOBb34pHgVkIr2nznjbp8eeszY/edit#gid=0"
+            >
+              Click here to get your own copy of the brackets!
+            </a>
+            <img className="bracketsImage" src={brackets} alt="brackets" />
+          </div>
+        </Tab.Panel>
+
+        <Tab.Panel className="tabpanel">
+          <h2>What Is This?</h2>
+          <div className="information">
+            <p>
+              In 2020, we held the first ever Conbini Tournament, pitting 64
+              conbini items in 4 different divisions against one another to
+              decide the best item at the conbini. The winner of that tournament
+              was none other than the legendary{' '}
+              <span className="emphasis">Famichiki.</span>
+            </p>
+            <br />
+            <p>
+              Now, in 2022, to coincide with the FIFA World Cup and to celebrate
+              100 episodes of our podcast, we have decided to hold another
+              tournament: the{' '}
+              <span className="emphasis">Conbini World Cup</span>
+            </p>
+            <br />
+            <p>
+              Each day for the next couple of weeks we will release a number
+              matches, and we need <span className="emphasis">YOUR</span> help
+              to decide the winners! All matches will be decided through polls
+              on twitter, so please click on the links add your votes to help us
+              crown the champion of the Conbini World Cup 2022!
+            </p>
           </div>
         </Tab.Panel>
       </Tab.Panels>
