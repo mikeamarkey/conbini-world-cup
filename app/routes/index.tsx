daecb64f463ee1fecb5110101100c858b72330f7
Michael Markey <michaelamarkey@gmail.com>
2022-12-19 08:58:40 -0500

    feat: prepare for release

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -6,6 +6,7 @@ import { Matchup, matchupStyles } from '~/components';
 import { getMatchups } from '~/models/matchup.server';
 import indexStyles from '~/styles/index.css';
 import brackets from '~/assets/brackets.png';
+import twitter from '~/assets/twitter.png';
 
 export const loader = async ({ context }: LoaderArgs) => {
   const cachedMatchups = await context.CWC_KV.get<
@@ -87,9 +88,16 @@ export default function Index() {
           <div className="brackets">
             <a
               className="bracketsLink"
-              href="https://docs.google.com/spreadsheets/d/1kyKhbq6h-8Qe_RWgavOBb34pHgVkIr2nznjbp8eeszY/edit#gid=0"
+              href="https://docs.google.com/spreadsheets/d/1NiAOum_dh0k0p4I6IgHyLRtUrEHqHxccBeGcJdHde90/edit#gid=0"
             >
-              Click here to get your own copy of the brackets!
+              Click here to see the latest brackets (updated daily)
+            </a>
+
+            <a
+              className="bracketsLink"
+              href="https://docs.google.com/spreadsheets/u/3/d/1kyKhbq6h-8Qe_RWgavOBb34pHgVkIr2nznjbp8eeszY/copy#gid=0"
+            >
+              Click here to download a fresh copy of your own!
             </a>
             <img className="bracketsImage" src={brackets} alt="brackets" />
           </div>
@@ -120,6 +128,22 @@ export default function Index() {
               on twitter, so please click on the links, add your votes, and help
               us crown the champion of the Conbini World Cup 2022!
             </p>
+
+            <a
+              className="informationLink"
+              href="https://twitter.com/conbiniboys"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <span className="informationLinkText">
+                Conbini Boys Twitter Page
+              </span>
+              <img
+                className="informationLinkIcon"
+                src={twitter}
+                alt="See the result"
+              />
+            </a>
           </div>
         </Tab.Panel>
       </Tab.Panels>
