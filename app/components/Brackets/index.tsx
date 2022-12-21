b1168ec62a783efc9d2db75711561325fbfe6d80
Michael Markey <michaelamarkey@gmail.com>
2022-12-21 06:08:04 -0500

    chore: update assets, add filled in brackets

--- app/components/Brackets/index.tsx
+++ app/components/Brackets/index.tsx
@@ -1,5 +1,7 @@
 import type { LinksFunction } from '@remix-run/cloudflare';
 import bracketsImage from '~/assets/brackets.png';
+import bracketsMattImage from '~/assets/brackets-matt.png';
+import bracketsMikeImage from '~/assets/brackets-mike.png';
 import styles from './styles.css';
 
 export const bracketsStyles: LinksFunction = () => {
@@ -14,26 +16,40 @@ export const bracketsStyles: LinksFunction = () => {
 export const Brackets = () => {
   return (
     <div className="brackets">
-      <h2>Check Out the Brackets</h2>
-      <div className="card bracketsContent">
-        <a
-          className="bracketsContentLink"
-          href="https://docs.google.com/spreadsheets/d/1NiAOum_dh0k0p4I6IgHyLRtUrEHqHxccBeGcJdHde90/edit#gid=0"
-        >
-          Click here to see the latest brackets (updated daily)
-        </a>
+      <div className="bracketsGroup">
+        <h2>Official Brackets</h2>
+        <div className="card bracketsContent">
+          <a
+            className="bracketsContentLink"
+            href="https://docs.google.com/spreadsheets/d/1NiAOum_dh0k0p4I6IgHyLRtUrEHqHxccBeGcJdHde90/edit#gid=0"
+          >
+            Click here to see the latest brackets (updated daily)
+          </a>
 
-        <a
-          className="bracketsContentLink"
-          href="https://docs.google.com/spreadsheets/u/3/d/1kyKhbq6h-8Qe_RWgavOBb34pHgVkIr2nznjbp8eeszY/copy#gid=0"
-        >
-          Click here to download a fresh copy of your own!
-        </a>
-        <img
-          className="bracketsContentImage"
-          src={bracketsImage}
-          alt="brackets"
-        />
+          <a
+            className="bracketsContentLink"
+            href="https://docs.google.com/spreadsheets/u/3/d/1kyKhbq6h-8Qe_RWgavOBb34pHgVkIr2nznjbp8eeszY/copy#gid=0"
+          >
+            Click here to download a fresh copy of your own!
+          </a>
+          <img
+            className="bracketsContentImage"
+            src={bracketsImage}
+            alt="brackets"
+          />
+        </div>
+      </div>
+      <div className="bracketsGroup">
+        <h2>Matt's Bracket</h2>
+        <div className="bracketsContent card">
+          <img src={bracketsMattImage} alt="brackets" />
+        </div>
+      </div>
+      <div className="bracketsGroup">
+        <h2>Mike's Bracket</h2>
+        <div className="bracketsContent card">
+          <img src={bracketsMikeImage} alt="brackets" />
+        </div>
       </div>
     </div>
   );
